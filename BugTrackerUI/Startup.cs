0978a1029caf893382478969b5616729327d5663
Francisco Ruiz Rivas <francisco@ruizrivas.net>
2020-07-20 23:25:06 -0300

    Working with Services and Data

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BugTrackerUI.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BugTrackerUI.Services;
 
 namespace BugTrackerUI
 {
@@ -28,6 +22,8 @@ namespace BugTrackerUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddSingleton<IBugService, BugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
